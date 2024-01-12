cf9dcc804ebe30ada14d962774d3045151806d46
Luisfer310 <luisosuna@MacBook-Pro-de-Luis.local>
2024-01-12 11:10:03 -0700

    Ultimo guardado 12/01/2024 11:09 hrs

--- Universidad Python/Tareas y Ejercicios/TE13.py
+++ Universidad Python/Tareas y Ejercicios/TE13.py
@@ -1,11 +1,11 @@
 edad = int(input('Ingresa tu edad y te diremos una frase sobre la etapa de la vida en la que estas: '))
 frase = None
 
-if edad >= 0 and edad <= 10:
+if (edad >= 0) and (edad <= 10):
     frase = 'La infancia es increíble…'
-elif edad >= 11 and edad <= 20:
+elif (edad >= 11) and (edad <= 20):
     frase = 'Muchos cambios y mucho estudio…'
-elif edad >= 21 and edad <= 30:
+elif (edad >= 21) and (edad <= 30):
     frase = 'Amor y comienza el trabajo…'
 else:
     print('Etapa de vida no reconocida')
